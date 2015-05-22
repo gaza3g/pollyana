ec76465dd3d876782a4103362b58378f89db19db
Ghazaly <gazaly@gmail.com>
2015-05-22 16:10:37 +0800

    Shitty auth working.

---
+++ Pollyana/Controllers/AuthController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pollyana.Controllers
+{
+    [AllowAnonymous]
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        [HttpGet]
+        public ActionResult LogIn(string returnUrl)
+        {
+            var model = new LoginModel
+            {
+                ReturnUrl = returnUrl
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult LogIn(LoginModel model)
+        {
+            if(!ModelState.IsValid)
+                return View();
+
+            if(model.Email == "admin@admin.com" && model.Password == "password")
+            {
+                var identity = new ClaimsIdentity(new[] {
+                    new Claim(ClaimTypes.Name, "Ben"),
+                    new Claim(ClaimTypes.Email, "a@b.com"),
+                    new Claim(ClaimTypes.Country, "England")
+                },
+                "ApplicationCookie");
+
+                var ctx = Request.GetOwinContext();
+                var authManager = ctx.Authentication;
+
+                authManager.SignIn(identity);
+
+                return Redirect(GetRedirectUrl(model.ReturnUrl));
+            }
+
+            ModelState.AddModelError("", "Invalid credentials");
+            return View();
+        }
+
+        public ActionResult LogOut()
+        {
+            var ctx = Request.GetOwinContext();
+            var authManager = ctx.Authentication;
+
+            authManager.SignOut("ApplicationCookie");
+            return RedirectToAction("index", "users");
+        }
+
+        private string GetRedirectUrl(string returnUrl)
+        {
+            if(string.IsNullOrEmpty(returnUrl) || !Url.IsLocalUrl(returnUrl))
+            {
+                return Url.Action("index", "user");
+            }
+
+            return returnUrl;
+        }
+    }
+
+}
+
+public class LoginModel
+{
+    [Required]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [HiddenInput]
+    public string ReturnUrl { get; set; }
+
+}
