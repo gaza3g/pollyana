2c4be43261723b0d8ab619b5ae955e2702e3bf13
Ghazaly <gazaly@gmail.com>
2015-05-25 16:17:02 +0800

    Scaffolded Polls model.

---
+++ Pollyana/Controllers/PollsController.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Pollyana;
+using Pollyana.Models;
+
+namespace Pollyana.Controllers
+{
+    public class PollsController : Controller
+    {
+        private AppDbContext db = new AppDbContext();
+
+        // GET: Polls
+        public ActionResult Index()
+        {
+            return View(db.Polls.ToList());
+        }
+
+        // GET: Polls/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Poll poll = db.Polls.Find(id);
+            if (poll == null)
+            {
+                return HttpNotFound();
+            }
+            return View(poll);
+        }
+
+        // GET: Polls/Create
+        public ActionResult Create()
+        {
+            Poll poll = new Poll();
+            poll.DateCreated = DateTime.Now;
+            poll.DateModified = DateTime.Now;
+            return View(poll);
+        }
+
+        // POST: Polls/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PollID,Title,DateCreated,DateModified,isOpen,AccessCode")] Poll poll)
+        {
+            if (ModelState.IsValid)
+            {
+                poll.DateCreated = DateTime.Now;
+                poll.DateModified = DateTime.Now;
+
+                using (var db = new AppDbContext())
+                { 
+                    var userStore = new UserStore<AppUser>(db);
+                    var userManager = new UserManager<AppUser>(userStore);
+                    poll.User = db.Users.FirstOrDefault(u => u.UserName == User.Identity.Name);
+                    db.Polls.Add(poll);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View(poll);
+        }
+
+        // GET: Polls/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Poll poll = db.Polls.Find(id);
+            if (poll == null)
+            {
+                return HttpNotFound();
+            }
+            return View(poll);
+        }
+
+        // POST: Polls/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PollID,Title,DateCreated,DateModified,isOpen,AccessCode")] Poll poll)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(poll).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(poll);
+        }
+
+        // GET: Polls/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Poll poll = db.Polls.Find(id);
+            if (poll == null)
+            {
+                return HttpNotFound();
+            }
+            return View(poll);
+        }
+
+        // POST: Polls/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Poll poll = db.Polls.Find(id);
+            db.Polls.Remove(poll);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
